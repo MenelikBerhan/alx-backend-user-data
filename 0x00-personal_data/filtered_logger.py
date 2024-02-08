9420b5c1523f6eb2b8d18c1bbe0bcea7df229c28
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-08 03:24:14 +0300

    Task 1: format a redacted log record

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Obfuscating log messages using regex substitution.
 """
-import re
 from typing import List
+import logging
+import re
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -12,3 +13,25 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = r'({})([^{}]*)'.format("|".join(fields), separator)
     replacement = r'\1={}'.format(redaction)
     return re.sub(pattern, replacement, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"       # replacement for self.fields' values
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"         # separator b/n each <field>=<value> pairs
+
+    def __init__(self, fields: List[str]):
+        """Initialize formatter with list of fields to redact."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields    # fields whose values are to be redacted
+
+    def format(self, record: logging.LogRecord) -> str:
+        """After redacting `fields` in the `record`'s message,
+        formats the specified record as text."""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
+            )
+        return super().format(record)
