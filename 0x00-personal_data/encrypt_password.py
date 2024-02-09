1e6d2e21d5130efbbd0ea08c4525eba163b11b5a
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-09 03:07:27 +0300

    Task 6: check given password using hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Hashing password using bcrypt.
 """
-import bcrypt
+import bcrypt       # type: ignore
 
 
 def hash_password(password: str) -> bytes:
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     # encode password bfr passing to `haspw`
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if `password` matches `hased_password`."""
+    # encode password bfr passing to `checkpw`
+    return bcrypt.checkpw(password.encode(), hashed_password)
