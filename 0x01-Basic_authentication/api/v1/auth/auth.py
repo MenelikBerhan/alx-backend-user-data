b84583b21307cf1818dcf8dbe861ee493fc264cc
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-12 21:17:58 +0300

    Task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+"""Class to manage API Authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+User = TypeVar('User')
+
+
+class Auth():
+    """Template for all authentication system in the API.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if authentication is required."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Extracts the authorize header from request."""
+        return None
+
+    def current_user(self, request=None) -> User:
+        """Returns the current user."""
+        return None
