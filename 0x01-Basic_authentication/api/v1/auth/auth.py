b86823ad6bacf962f0adc4ed88417a6a41da4811
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-12 21:48:29 +0300

    Use given function definitions wiz anotation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,19 @@
 """Class to manage API Authentication
 """
 from flask import request
-
+from typing import List, TypeVar
 
 class Auth():
     """Template for all authentication system in the API.
     """
-    def require_auth(self, path, excluded_paths):
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if authentication is required."""
         return False
 
-    def authorization_header(self, request=None):
+    def authorization_header(self, request=None) -> str:
         """Extracts the authorize header from request."""
         return None
 
-    def current_user(self, request=None):
+    def current_user(self, request=None) -> TypeVar('User'):
         """Returns the current user."""
         return None
