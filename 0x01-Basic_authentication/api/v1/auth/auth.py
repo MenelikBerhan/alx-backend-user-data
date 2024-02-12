d414ecb98e246d1bcb2c9765269d6db0bc4ebaee
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-13 01:42:24 +0300

    Task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Class to manage API Authentication
 """
 from flask import request
+from re import match
 from typing import List, TypeVar, Union
 
 
@@ -9,14 +10,18 @@ class Auth:
     """Template for all authentication system in the API.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if authentication is required. Returns `True`if `path`
-        is not in `excluded_paths`, otherwise returns `False`."""
+        """Checks if authentication is required. Returns `True` if `path`
+        is not in `excluded_paths`, otherwise returns `False`.
+        Excluded paths can contain the regex character '*' at the end."""
         if path is None or not excluded_paths:
             return True
         if not path.endswith('/'):
             path += '/'
         # return True if path is not in excluded_paths
-        return not bool(excluded_paths.count(path))
+        for excl_path in excluded_paths:
+            if match(excl_path, path) is not None:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> Union[str, None]:
         """Returns the `Authorization` header value in `request`."""
