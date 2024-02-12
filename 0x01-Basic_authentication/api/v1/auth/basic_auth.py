ffd65b100b93cffa89e1b8f19adf5bb0d51e722f
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-13 00:12:58 +0300

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Basic authentication
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+from binascii import Error as DecodingError
 from typing import Union
 
 
@@ -16,3 +18,15 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> Union[str, None]:
+        """Returns the decoded value of the Base64
+        string `base64_authorization_header`"""
+        if base64_authorization_header is None or\
+                type(base64_authorization_header) != str:
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except DecodingError:
+            return None
