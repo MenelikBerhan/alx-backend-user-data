b89b012a8dba13924d6a0d5323abc2426b2d0d9b
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-12 23:50:11 +0300

    Task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None     # stores the type of authentication to use
-if getenv('AUTH_TYPE') == 'auth':
+if getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
