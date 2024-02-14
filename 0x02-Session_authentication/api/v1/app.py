97d815d41f0b097023e81387afd1088ee0235b2e
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-15 00:35:05 +0300

    Set current user in request if its not None

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,9 +44,10 @@ def authentication_filter():
     # if getenv('AUTH_TYPE') == 'session_auth' and\
     #         auth.session_cookie(request) is None:
     #     abort(401)
-    request.current_user = auth.current_user(request)
-    if request.current_user is None:
+    current_user = auth.current_user(request)
+    if current_user is None:
         abort(403)
+    request.current_user = current_user
 
 
 @app.errorhandler(404)
