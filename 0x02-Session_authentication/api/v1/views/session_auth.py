dfc41bb44a53293d0016fa7b10b957c56fb5965d
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-15 00:29:42 +0300

    Revert changes

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import request, jsonify, session
+from flask import request, jsonify
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
@@ -36,5 +36,4 @@ def session_login():
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
     response.set_cookie(getenv('SESSION_NAME'), session_id)
-    session[getenv('SESSION_NAME')] = session_id
     return response
