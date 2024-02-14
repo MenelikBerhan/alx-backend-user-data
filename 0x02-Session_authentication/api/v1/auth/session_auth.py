6d3bb3bfa90572c5efed1e6b936d6ab4397fe17b
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-14 22:16:53 +0300

    Task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,19 @@ from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
 from typing import Tuple, TypeVar, Union
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Implements a session authentication."""
-    pass
+    # stores `<session_id>:<user_id>` key, value pairs
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> Union[str, None]:
+        """Creates a session id for `user_id` and stores it in
+        `user_id_by_session_id`. Returns the created session id."""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
