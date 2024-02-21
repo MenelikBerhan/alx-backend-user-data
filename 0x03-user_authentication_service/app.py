7eede6debaaae722d7d6d36b60fc670f7c6104c9
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-21 08:29:23 +0300

    Task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic Flask app
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'], strict_slashes=True)
+@app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
     """Creates and a user using `email` and `password` in request."""
     email = request.form.get('email')
@@ -26,6 +26,20 @@ def users():
         return jsonify({"message": "email already registered"}), 400
     else:
         return jsonify({"email": email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """If a user with given email & password exists creates a session id."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email=email, password=password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == '__main__':
