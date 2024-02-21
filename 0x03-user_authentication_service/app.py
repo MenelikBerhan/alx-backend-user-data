bfa9da2dd3770f41b656082b31ea97c237108257
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-21 09:42:24 +0300

    Task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,5 +61,16 @@ def profile():
     return jsonify(email=user.email)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """Generates reset_token for a user of given email in request form."""
+    email = request.form.get('email')
+    try:
+        new_reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": new_reset_token})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
