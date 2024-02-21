de380de1037efbcc1c947b9ca1572399dc4500a6
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-21 09:55:08 +0300

    Task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,3 +86,16 @@ class Auth:
         new_reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=new_reset_token)
         return new_reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """If a user with given `reset_token` exists, updates its password.
+
+        Raises:
+            `ValueError` if no user could be found."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        self._db.update_user(user.id,
+                             hashed_password=_hash_password(password),
+                             reset_token=None)
